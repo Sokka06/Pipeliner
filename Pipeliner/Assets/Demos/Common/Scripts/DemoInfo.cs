af861c0e8f04ab67f41645b0aed6e0c57940f73c
Sokka06 <Sokka06@gmail.com>
2022-03-18 12:20:44 +0200

    added folders, demo scene template

--- Pipeliner/Assets/Demos/Common/Scripts/DemoInfo.cs
+++ Pipeliner/Assets/Demos/Common/Scripts/DemoInfo.cs
@@ -7,9 +7,9 @@ namespace Demos.Common
     [CreateAssetMenu(fileName = "Demo Info", menuName = "Demos/Info")]
     public class DemoInfo : ScriptableObject
     {
-        public string Title;
-        public string Subtitle;
+        public string Title = "Demo";
+        public string Subtitle = "Subtitle";
         [TextArea(4, 8)]
-        public string Description;
+        public string Description = "Description";
     }
 }
